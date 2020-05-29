c516d2a55ad40a2d91aaefedd7caa739e66e7816
Costa Michailidis <costa@innovationbound.com>
2020-05-29 10:24:41 -0400

    I hope this works

--- sw.js
+++ sw.js
@@ -5,8 +5,17 @@ var cache = 'tao-te-ching';
 var files = [
   'index.html',
   'fira-mono.woff2',
-  'main.css',
-  'yin-yang.jpg'
+  'android-chrome-192x192.png',
+  'android-chrome-512x512.png',
+  'apple-touch-icon.png',
+  'browserconfig.xml',
+  'favicon.ico',
+  'favicon-16x16.png',
+  'favicon-32x32.png',
+  'manifest.json',
+  'mstile-150x150.png',
+  'safari-pinned-tab.svg',
+  'site.webmanifest'
 ]
 
 self.addEventListener('install', function(event) {
